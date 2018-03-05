d2d6960960293a59a50c4aa461e2353784e51f6f
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-03-05 16:12:26 -0500

    created 2 GET endpoints

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 'use strict';
 
-// TEMP: Simple In-Memory Database
+const express = require('express');
 const data = require('./db/notes');
 
-console.log('hello world!');
+const app = express();
+app.use(express.static('public'));
 
-// INSERT EXPRESS APP CODE HERE...
+
+app.get('/api/notes', (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  const dataByFilter = data.filter(item => item.title.includes(searchTerm));
+  res.json(dataByFilter);
+
+});
+
+
+app.get('/api/notes/:Id', (req, res) => {
+  const Id = req.params.Id;
+  const noteById = data.find(item => item.id === Number(Id));
+  res.json(noteById);
+});
+
+// Listen for incoming connections
+app.listen(8080, function(){
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
